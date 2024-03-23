896c83bc0491a1fbcbcc3672c9f595facf01b7f4
Ridha <146313280+imridd@users.noreply.github.com>
2024-03-23 19:38:51 +0530

    Add files via upload

---
+++ Practice after long break.py
@@ -0,0 +1,16 @@
+#1. Write a program that takes two numbers as input and prints their sum, difference, product, and quotient.
+num_1=float(input("Enter a number : "))
+num_2=float(input("Enter another number : "))
+sum=num_1+num_2
+difference=num_2-num_1
+mul=num_1*num_2
+quotient=num_2/num_1
+print(f"{num_1}+{num_2} = ",sum)
+print(f"{num_2}-{num_1} = ",difference)
+print(f"{num_1}*{num_2} = ",mul)
+print(f"{num_2}/{num_1} = ",quotient)
+
+
+
+
+
